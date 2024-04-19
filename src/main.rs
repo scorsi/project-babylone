82153d296586a40cd877eaddecba3cf319028d15
Sylvain Corsini <sylvain.corsini@protonmail.com>
2024-04-19 21:41:37 +0200

    feat: init project

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::prelude::*;
+use bevy::window::close_on_esc;
+
+const WW: f32 = 1200.0;
+const WH: f32 = 900.0;
+const BG_COLOR: (u8, u8, u8) = (25, 20, 43);
+
+fn main() {
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        resizable: true,
+                        focused: true,
+                        resolution: (WW, WH).into(),
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
+        .insert_resource(ClearColor(Color::rgb_u8(
+            BG_COLOR.0, BG_COLOR.1, BG_COLOR.2,
+        )))
+        .insert_resource(Msaa::Off)
+        .add_plugins(LogDiagnosticsPlugin::default())
+        .add_plugins(FrameTimeDiagnosticsPlugin)
+        .add_systems(Startup, setup)
+        .add_systems(Update, close_on_esc)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
