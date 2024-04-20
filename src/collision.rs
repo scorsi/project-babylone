f82df54dbc6e5131dff2fd89162bb278f62fe204
Sylvain Corsini <sylvain.corsini@protonmail.com>
2024-04-20 17:53:21 +0200

    feat: optimize collisions using kdtree
    closes #13

--- src/collision.rs
+++ src/collision.rs
@@ -1,36 +1,91 @@
+use std::time::Duration;
 use bevy::prelude::*;
-use crate::consts::BULLET_DAMAGE;
+use bevy::time::common_conditions::on_timer;
+use kd_tree::{KdPoint, KdTree};
+
+use crate::consts::*;
 use crate::enemy::Enemy;
 use crate::gun::Bullet;
-
 use crate::state::GameState;
 
 pub struct CollisionPlugin;
 
+#[derive(Component)]
+struct Collidable {
+    pos: Vec2,
+    entity: Entity,
+}
+
+impl KdPoint for Collidable {
+    type Scalar = f32;
+    type Dim = typenum::U2;
+    fn at(&self, k: usize) -> f32 {
+        if k == 0 {
+            return self.pos.x;
+        }
+
+        self.pos.y
+    }
+}
+
+#[derive(Resource)]
+struct EnemyKdTree(KdTree<Collidable>);
+
+impl Default for EnemyKdTree {
+    fn default() -> Self {
+        Self(KdTree::build_by_ordered_float(vec![]))
+    }
+}
+
 impl Plugin for CollisionPlugin {
     fn build(&self, app: &mut App) {
         app
+            .insert_resource(EnemyKdTree::default())
             .add_systems(
                 Update,
                 (
                     // handle_player_enemy_collision,
                     handle_enemy_bullet_collision,
+                    update_enemy_kd_tree.run_if(on_timer(Duration::from_secs_f32(KD_TREE_REFRESH_RATE))),
                 )
                     .run_if(in_state(GameState::InGame)),
             );
     }
 }
 
+fn update_enemy_kd_tree(
+    mut tree: ResMut<EnemyKdTree>,
+    enemy_query: Query<(&Transform, Entity), With<Enemy>>,
+) {
+    let mut items = Vec::new();
+    for (transform, entity) in enemy_query.iter() {
+        items.push(Collidable {
+            entity,
+            pos: transform.translation.truncate(),
+        })
+    }
+
+    tree.0 = KdTree::build_by_ordered_float(items);
+}
+
 fn handle_enemy_bullet_collision(
     mut commands: Commands,
     bullet_query: Query<(&Transform, Entity), With<Bullet>>,
-    mut enemy_query: Query<(&Transform, &mut Enemy), (With<Enemy>, Without<Bullet>)>,
+    tree: Res<EnemyKdTree>,
+    mut enemy_query: Query<(&Transform, &mut Enemy), With<Enemy>>,
 ) {
-    for (bullet_transform, bullet_entity) in bullet_query.iter() {
-        for (enemy_transform, mut enemy) in enemy_query.iter_mut() {
-            if bullet_transform.translation.distance_squared(enemy_transform.translation) <= 500.0 {
+    if bullet_query.is_empty() || enemy_query.is_empty() {
+        return;
+    }
+
+    for (bullet_transform, entity) in bullet_query.iter() {
+        let pos = bullet_transform.translation;
+        let enemies = tree.0.within_radius(&[pos.x, pos.y], 25.0);
+
+        for e in enemies {
+            if let Ok((_, mut enemy)) = enemy_query.get_mut(e.entity) {
                 enemy.health -= BULLET_DAMAGE;
-                commands.entity(bullet_entity).despawn();
+                commands.entity(entity).despawn();
             }
         }
     }
--- src/consts.rs
+++ src/consts.rs
@@ -15,12 +15,12 @@ pub const BULLET_Z_INDEX: f32 = 1.0;
 pub const WORLD_DECORATION_Z_INDEX: f32 = 0.0;
 pub const ENEMY_Z_INDEX: f32 = 9.0;
 
-pub const BULLET_SPAWN_INTERVAL: f32 = 0.22;
+pub const BULLET_SPAWN_INTERVAL: f32 = 0.25;
 pub const BULLET_SPEED: f32 = 700.0;
-pub const BULLET_DAMAGE: f32 = 25.0;
+pub const BULLET_DAMAGE: f32 = 15.0;
 pub const BULLET_LIFETIME: f32 = 0.5;
-pub const NUM_BULLETS_PER_SHOT: usize = 25;
-pub const BULLET_MAX_SPREADING_ANGLE: f32 = 0.25;
+pub const NUM_BULLETS_PER_SHOT: usize = 5;
+pub const BULLET_MAX_SPREADING_ANGLE: f32 = 0.15;
 
 pub const WORLD_W: f32 = 3000.0;
 pub const WORLD_H: f32 = 3000.0;
@@ -31,6 +31,8 @@ pub const PLAYER_SPEED: f32 = 300.0;
 
 pub const MAX_NUM_ENEMIES: usize = 10000;
 pub const ENEMY_SPAWN_INTERVAL: f32 = 1.0;
-pub const ENEMY_SPAWN_RATE_PER_SECOND: usize = 250;
+pub const ENEMY_SPAWN_RATE_PER_SECOND: usize = 2;
 pub const ENEMY_SPEED: f32 = 225.0;
 pub const ENEMY_MAX_HEALTH: f32 = 100.0;
+
+pub const KD_TREE_REFRESH_RATE: f32 = 0.1;
